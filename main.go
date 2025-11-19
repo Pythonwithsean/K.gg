eee507d84478664adfdbd1eb16cbda71c04858bb
Pythonwithsean <pythonwithsean@gmail.com>
2025-11-19 23:30:12 +0000

    feat: add,del,put commands

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ import (
 )
 
 const ADDR = "localhost"
+
 const PORT = "8080"
+
+type Server struct {
+	addr  string
+	port  string
+	store *InMemoryStore
+}
+
+type command int
 
 type Store interface {
 	get(key string) string
@@ -23,10 +32,12 @@ type InMemoryStore struct {
 	mu    *sync.RWMutex
 }
 
-type FoundResult struct {
-	Found bool
-	Index int
-}
+const (
+	GET command = iota
+	PUT
+	DEL
+	UNKNOWN
+)
 
 func NewInMemoryStore() *InMemoryStore {
 	return &InMemoryStore{store: make(map[string]string), mu: &sync.RWMutex{}}
@@ -53,18 +64,42 @@ func (s *InMemoryStore) del(key string) {
 	delete(s.store, key)
 }
 
-type Server struct {
-	addr  string
-	port  string
-	store *InMemoryStore
-}
-
 func NewServer(addr, port string) *Server {
 	return &Server{
 		addr:  addr,
 		port:  port,
 		store: NewInMemoryStore(),
 	}
+}
+
+func parseCommand(line string) command {
+	switch cmd := strings.ToUpper(strings.Split(line, " ")[0]); cmd {
+	case "GET":
+		return GET
+	case "PUT":
+		return PUT
+	case "DEL":
+		return DEL
+	default:
+		return UNKNOWN
+	}
+}
+
+func parsePut(line string) (string, string, bool) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return "", "", false
+	}
+	return parts[1], parts[2], true
+
+}
+
+func parseGetOrDel(line string) (string, bool) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 2 {
+		return "", false
+	}
+	return parts[1], true
 }
 
 func (server *Server) handleConnection(conn net.Conn) {
@@ -74,37 +109,38 @@ func (server *Server) handleConnection(conn net.Conn) {
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		parts := strings.Split(line, " ")
-		if len(parts) == 0 {
-			continue
-		}
-		cmd := strings.ToUpper(parts[0])
-		switch cmd {
-		case "GET":
-			log.Printf("[info] handling a connection %s request!\n", cmd)
-			found := FoundResult{false, -1}
-			for i, part := range parts {
-				if strings.Contains(part, "?key=") {
-					found.Found = true
-					found.Index = i
-				}
-			}
-
-			if !found.Found || found.Index < 0 {
+		switch cmd := parseCommand(line); cmd {
+		case GET:
+			key, ok := parseGetOrDel(line)
+			if !ok {
+				conn.Write([]byte("Invalid get command"))
 				continue
 			}
-			p := strings.Split(parts[found.Index], "=")
-			key := p[len(p)-1]
 			if value, ok := server.store.get(key); ok {
 				conn.Write([]byte(fmt.Sprintf("%s\n", value)))
 			} else {
-				conn.Write([]byte(fmt.Sprintf("[err] %s not found\n", key)))
+				conn.Write([]byte(fmt.Sprintf("key %s not found\n", key)))
 			}
-		case "POST":
-			log.Println("[POST]")
-		case "DELETE":
-			log.Println("[DEL]")
+		case PUT:
+			key, val, ok := parsePut(line)
 
+			if !ok {
+				// handle
+				conn.Write([]byte("Invalid put command"))
+				continue
+			}
+			server.store.put(key, val)
+		case DEL:
+			key, ok := parseGetOrDel(line)
+			if !ok {
+				// handle
+				conn.Write([]byte("Invalid del command"))
+				continue
+			}
+			server.store.del(key)
+		case UNKNOWN:
+			// unknown command
+			log.Println("Unknwon command")
 		}
 	}
 }
